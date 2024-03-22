bee85c9e1246014cad96bfecf399a993573e1ee9
Vi-lka <vitalya.permyakov155@gmail.com>
2024-03-22 15:16:56 +0700

    feat: Entrance block

--- src/app/(mainPageBlocks)/Entrance.tsx
+++ src/app/(mainPageBlocks)/Entrance.tsx
@@ -3,12 +3,56 @@ import { TypographyH1 } from '@/components/typography'
 import React from 'react'
 
 export default function Entrance() {
+
+    const data = [
+        {
+            id: 0,
+            image: "/preview/1.jpg",
+            mainCode: "09.03.03",
+            mainName: "Прикладная информатика",
+            subCode: "09.03.03.35",
+            subName: "Прикладная информатика в искусстве и интерактивных медиа",
+        },
+        {
+            id: 1,
+            image: "/preview/2.jpg",
+            mainCode: "42.03.01",
+            mainName: "Реклама и связи с общественностью",
+            subCode: "42.03.01.31",
+            subName: "Рекламный маркетинг",
+        },
+        {
+            id: 2,
+            image: "/preview/3.jpg",
+            mainCode: "46.03.01",
+            mainName: "История",
+            subCode: "46.03.01.30",
+            subName: "История",
+        },
+        {
+            id: 3,
+            image: "/preview/4.jpg",
+            mainCode: "46.03.02",
+            mainName: "Документоведение и архивоведение",
+            subCode: "46.03.02.30",
+            subName: "Документоведение и архивоведение",
+        },
+        {
+            id: 4,
+            image: "/preview/5.jpg",
+            mainCode: "47.03.01",
+            mainName: "Философия",
+            subCode: "47.03.01.30",
+            subName: "Философия",
+        },
+    ]
+
     return (
         <section className='w-full'>
-            <TypographyH1 className='font-semibold text-primary mb-8 px-2'>
+            <TypographyH1 className='font-semibold text-primary mb-4 px-2'>
                 Поступай в Гуманитарный!
             </TypographyH1>
-            <CarouselComp />
+            <CarouselComp data={data} hrefTo='entrance' />
         </section>
     )
 }
--- src/components/CarouselComp.tsx
+++ src/components/CarouselComp.tsx
@@ -5,8 +5,24 @@ import type { CarouselApi} from './ui/carousel';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
 import { Card, CardContent } from './ui/card'
 import { cn } from '@/lib/utils';
+import Image from "next/image"
+import Link from 'next/link';
+import { Button } from './ui/button';
 
-export default function CarouselComp() {
+export default function CarouselComp({
+    hrefTo,
+    data
+}: {
+    hrefTo: string,
+    data: {
+        id: number;
+        image: string;
+        mainCode: string;
+        mainName: string;
+        subCode: string;
+        subName: string;
+    }[]
+}) {
     const [api, setApi] = React.useState<CarouselApi>()
     const [current, setCurrent] = React.useState(0)
     const [count, setCount] = React.useState(0)
@@ -45,15 +61,39 @@ export default function CarouselComp() {
         <div>
             <Carousel 
                 setApi={setApi} 
-                opts={{ skipSnaps: true }} 
+                opts={{ skipSnaps: window.innerWidth < 640 ? false : true }} 
                 className="w-full"
             >
                 <CarouselContent className='lg:-ml-8 -ml-4'>
-                    {Array.from({ length: 6 }).map((_, index) => (
-                        <CarouselItem key={index} className='lg:basis-1/3 sm:basis-1/2 lg:pl-8 pl-4'>
-                            <Card className=' aspect-[47/50] border-none shadow-md'>
-                                <CardContent className="flex w-full h-full items-center justify-center p-6">
-                                    <span className="text-4xl font-semibold">{index + 1}</span>
+                    {data.map(item => (
+                        <CarouselItem key={item.id} className='lg:basis-1/3 sm:basis-1/2 lg:pl-8 pl-4'>
+                            <Card className='h-full border-none shadow-md rounded-2xl'>
+                                <CardContent className="w-full h-full flex flex-col xl:gap-8 gap-6 md:justify-normal justify-between p-3">
+                                    <Image 
+                                        src={item.image}
+                                        alt="Image"
+                                        width={400}
+                                        height={140}
+                                        className='w-full object-cover rounded-xl h-[30%]'
+                                    />
+
+                                    <div className='flex flex-col justify-between gap-3 xl:px-8 px-5 text-primary xl:text-base text-sm'>
+                                        <div>
+                                            <p>{item.mainCode}</p>
+                                            <p>{item.mainName}</p>
+                                        </div>
+                                        <div>
+                                            <p>{item.subCode}</p>
+                                            <p className='font-bold'>{item.subName}</p>
+                                        </div>
+                                    </div>
+
+
+                                    <Link href={`/${hrefTo}/${item.id}`} className='w-fit mx-auto mb-3 md:mt-auto'>
+                                        <Button className='uppercase font-medium px-10 py-5 rounded-full hover:bg-background hover:text-accent'>
+                                            Подробнее
+                                        </Button>
+                                    </Link>
                                 </CardContent>
                             </Card>
                         </CarouselItem>
