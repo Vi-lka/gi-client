e8eb586419f14a8bbb714d169cb8e0f73622a23a
Vi-lka <vitalya.permyakov155@gmail.com>
2024-06-18 14:05:11 +0700

    fix: error page router refresh and back

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -17,58 +17,62 @@ export default function Error({
   reset: () => void,
 }) {
 
-    const { toast } = useToast();
-    const router = useRouter();
+  const { toast } = useToast();
+  const router = useRouter();
 
-    useEffect(() => {
-        Sentry.captureException(error);
-
-        console.log("Error: ", error.message);
-    
-        toast({
-          variant: "destructive",
-          title: "Ошибка! Что-то пошло не так:",
-          description: (
-            <p>
-              {getShortText(error.message)}
-            </p>
-          ),
-          className: "font-Raleway",
-          action: (
-            <ToastAction
-              className="px-2 py-6 text-sm"
-              altText={"Попробовать снова"}
-              onClick={() => reset()}
-            >
-              <Repeat className="h-8 w-8" />
-            </ToastAction>
-          ),
-        });
-    }, [error, reset, toast])
+  useEffect(() => {
+    Sentry.captureException(error);
+    console.log("Error: ", error.message);
+  
+    toast({
+      variant: "destructive",
+      title: "Ошибка! Что-то пошло не так:",
+      description: (
+        <p>
+          {getShortText(error.message)}
+        </p>
+      ),
+      className: "font-Raleway",
+      action: (
+        <ToastAction
+          className="px-2 py-6 text-sm"
+          altText={"Попробовать снова"}
+          onClick={() => {
+            reset()
+            router.refresh()
+          }}
+        >
+          <Repeat className="h-8 w-8" />
+        </ToastAction>
+      ),
+    });
+  }, [error, reset, router, toast])
  
-    return (
-        <div className='font-Din relative flex flex-col justify-center min-h-screen bg-background'>
-          <div className="mx-auto my-10 flex flex-col items-center gap-10 text-center">
-              <div className="flex flex-col items-center gap-4 text-center">
-                  <CircleAlert size={36} />
-
-                  <h2 className="font-Cera text-3xl font-bold uppercase">
-                      Ошибка
-                  </h2>
-                      
-                  <p className="text-sm font-normal">
-                      {getShortText(error.message)}
-                  </p>
-              </div>
-                      
-              <Button
-                  className="w-full max-w-[240px] p-6 uppercase hover:bg-background hover:text-primary rounded-3xl"
-                  onClick={() => router.back()}
-              >
-                  Вернуться
-                  <Undo2 className="ml-1" size={18} />
-              </Button>
-          </div>
+  return (
+    <div className='font-Din relative flex flex-col justify-center min-h-screen bg-background'>
+      <div className="mx-auto my-10 flex flex-col items-center gap-10 text-center">
+        <div className="flex flex-col items-center gap-4 text-center">
+          <CircleAlert size={36} />
+          <h2 className="font-Cera text-3xl font-bold uppercase">
+            Ошибка
+          </h2>
+              
+          <p className="text-sm font-normal">
+            {getShortText(error.message)}
+          </p>
         </div>
-    )
+                
+        <Button
+          className="w-full max-w-[240px] p-6 uppercase hover:bg-background hover:text-primary rounded-3xl"
+          onClick={() => {
+            reset()
+            router.back()
+          }}
+        >
+          Вернуться
+          <Undo2 className="ml-1" size={18} />
+        </Button>
+      </div>
+    </div>
+  )
 }
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -5,6 +5,7 @@ import { getShortText } from '@/lib/utils'
 import { CircleAlert, Repeat } from 'lucide-react'
 import { useEffect } from 'react'
 import * as Sentry from "@sentry/nextjs";
+import { useRouter } from 'next/navigation'
  
 export default function GlobalError({
   error,
@@ -13,38 +14,39 @@ export default function GlobalError({
   error: Error & { digest?: string }
   reset: () => void
 }) {
+  const router = useRouter();
 
-    useEffect(() => {
-        Sentry.captureException(error);
+  useEffect(() => {
+    Sentry.captureException(error);
 
-        console.log("GlobalError: ", error.message);
-    
-    }, [error, reset])
+    console.log("GlobalError: ", error.message);
+  }, [error, reset])
  
-    return (
-      <html>
-        <body className='font-Din relative flex flex-col justify-between min-h-screen bg-background'>
-          <div className="mx-auto my-10 flex flex-col items-center gap-10 text-center">
-              <div className="flex flex-col items-center gap-4 text-center">
-                  <CircleAlert size={36} />
-
-                  <h2 className="font-Cera text-3xl font-bold uppercase">
-                      Error
-                  </h2>
-                      
-                  <p className="text-sm font-normal">
-                      {getShortText(error.message)}
-                  </p>
-              </div>
-                      
-              <Button
-                  className="w-full max-w-[240px] p-6 uppercase hover:bg-background hover:text-primary rounded-3xl"
-                  onClick={() => reset()}
-              >
-                  <Repeat className="ml-1" size={18} />
-              </Button>
+  return (
+    <html>
+      <body className='font-Din relative flex flex-col justify-between min-h-screen bg-background'>
+        <div className="mx-auto my-10 flex flex-col items-center gap-10 text-center">
+          <div className="flex flex-col items-center gap-4 text-center">
+            <CircleAlert size={36} />
+            <h2 className="font-Cera text-3xl font-bold uppercase">
+              Error
+            </h2>
+            <p className="text-sm font-normal">
+              {getShortText(error.message)}
+            </p>
           </div>
-        </body>
-      </html>
-    )
+
+          <Button
+            className="w-full max-w-[240px] p-6 uppercase hover:bg-background hover:text-primary rounded-3xl"
+            onClick={() => {
+              reset()
+              router.refresh()
+            }}
+          >
+            <Repeat className="ml-1" size={18} />
+          </Button>
+        </div>
+      </body>
+    </html>
+  )
 }
