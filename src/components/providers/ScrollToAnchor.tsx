47ad300ab033281fb1c4464baab2e4c4fabb9a8e
Vi-lka <vitalya.permyakov155@gmail.com>
2024-11-19 13:56:41 +0700

    fix: scroll to provider

--- src/components/providers/ScrollToAnchor.tsx
+++ src/components/providers/ScrollToAnchor.tsx
@@ -8,17 +8,15 @@ export default function ScrollToAnchor({
     children: React.ReactNode
 }) {
     useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const observer = new ResizeObserver(() => {
-                const { hash } = window.location
-                const elem = hash.length > 0 && document.querySelector(decodeURI(hash));
-                elem && elem.scrollIntoView({ behavior: "smooth" });
-            });
-    
-            window.addEventListener("resize", () => observer.unobserve(document.body));
-            observer.observe(document.body);
-            return () => observer.unobserve(document.body);
-        }
+        const observer = new ResizeObserver(() => {
+            const { hash } = window.location
+            const elem = hash.length > 0 && document.querySelector(decodeURI(hash));
+            elem && elem.scrollIntoView({ behavior: "smooth" });
+        });
+
+        window.addEventListener("resize", () => observer.unobserve(document.body));
+        observer.observe(document.body);
+        return () => observer.unobserve(document.body);
     }, []);
 
     return <>{children}</>
