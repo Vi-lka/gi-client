a07903ce1bed5be61e68c7443b62347cf4dc6e0f
Vi-lka <vitalya.permyakov155@gmail.com>
2024-11-19 12:16:05 +0700

    fix: webpack error

--- next.config.js
+++ next.config.js
@@ -25,18 +25,18 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.resolve.fallback = {
+  // webpack(config) {
+  //   config.resolve.fallback = {
 
-      // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped.
-      ...config.resolve.fallback,  
+  //     // if you miss it, all the other options in fallback, specified
+  //     // by next.js will be dropped.
+  //     ...config.resolve.fallback,  
 
-      fs: false, // the solution
-    };
+  //     fs: false, // the solution
+  //   };
     
-    return config;
-  },
+  //   return config;
+  // },
   async rewrites() {
     return [
       {
